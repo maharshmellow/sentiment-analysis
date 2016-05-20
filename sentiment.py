d16add8f2cf0501d4e1359454f35786f700e6c01
maharshmellow <maharsh@live.ca>
2016-05-19 23:21:15 -0600

    added imports

---
+++ sentiment.py
@@ -0,0 +1,10 @@
+# necessary libraries: nltk
+
+from nltk.classify import NaiveBayesClassifier
+from nltk.corpus import subjectivity
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
+
+n_instances = 100
+# subj_docs = [(sent, 'subj') for sent in subjectivity.sents(categories="subj")[:n_instances]]
+
