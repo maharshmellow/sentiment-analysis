698c1dcda4e94552458b14250bcb04bffc123333
maharshmellow <maharsh@live.ca>
2016-05-20 21:05:00 -0600

    fixed commas to be consistent

--- sentiment.py
+++ sentiment.py
@@ -6,7 +6,7 @@ from nltk.sentiment import SentimentAnalyzer
 from nltk.sentiment.util import *
 
 n_instances = 100
-subj_docs = [(sent, 'subj') for sent in subjectivity.sents(categories="subj")[:n_instances]]
-obj_docs = [(sent, 'obj') for sent in subjectivity.sents(categories="obj")[:n_instances]]
+subj_docs = [(sent, "subj") for sent in subjectivity.sents(categories="subj")[:n_instances]]
+obj_docs = [(sent, "obj") for sent in subjectivity.sents(categories="obj")[:n_instances]]
 print(len(subj_docs), len(obj_docs))
 
