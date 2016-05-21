2c6d7906f7f8b5fdab3102e3b7abaa9fd4243a25
maharshmellow <maharsh@live.ca>
2016-05-20 22:31:23 -0600

    testing/training split

--- sentiment.py
+++ sentiment.py
@@ -10,3 +10,13 @@ subj_docs = [(sent, "subj") for sent in subjectivity.sents(categories="subj")[:n
 obj_docs = [(sent, "obj") for sent in subjectivity.sents(categories="obj")[:n_instances]]
 print(len(subj_docs), len(obj_docs))
 
+print(subj_docs[0]) # shows the first tokenized subjective movie review
+
+
+train_subj_docs = subj_docs[:80]
+test_subj_docs = subj_docs[80:100]
+train_obj_docs = obj_docs[:80]
+test_obj_docs = obj_docs[80:100]
+
+training_docs = train_subj_docs + train_obj_docs
+testing_docs = test_subj_docs + test_obj_docs
