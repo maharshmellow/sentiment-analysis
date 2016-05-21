72996cbca700d2938fe3206acfc3430f0eaac66e
maharshmellow <maharsh@live.ca>
2016-05-20 22:32:08 -0600

    mark negation using suffix

--- sentiment.py
+++ sentiment.py
@@ -20,3 +20,6 @@ test_obj_docs = obj_docs[80:100]
 
 training_docs = train_subj_docs + train_obj_docs
 testing_docs = test_subj_docs + test_obj_docs
+
+sentim_analyzer = SentimentAnalyzer()
+all_words_neg = sentim_analyzer.all_words([mark_negation(doc) for doc in training_docs])        # mark_negation puts a _NEG suffix to words that appear between a negation and a punction mark
