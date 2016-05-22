bbd8bd26235f900ce79e971bdf16b364e6e74339
maharshmellow <maharsh@live.ca>
2016-05-21 21:29:15 -0600

    changed sentiment to access ec2

--- sentiment.py
+++ sentiment.py
@@ -1,38 +1,16 @@
-# necessary libraries: nltk
+import urllib.parse as up
+import urllib.request
 
-from nltk.classify import NaiveBayesClassifier
-from nltk.corpus import subjectivity
-from nltk.sentiment import SentimentAnalyzer
-from nltk.sentiment.util import *
+sentence = "dumb cow"
 
-n_instances = 500
-subj_docs = [(sent, "subj") for sent in subjectivity.sents(categories="subj")[:n_instances]]
-obj_docs = [(sent, "obj") for sent in subjectivity.sents(categories="obj")[:n_instances]]
-print(len(subj_docs), len(obj_docs))
+if sentence:
+    argument = up.quote_plus(sentence)         # formats it as a proper url argument
+    finalUrl = "http://ec2-54-191-21-155.us-west-2.compute.amazonaws.com/cgi-bin/sentiment.cgi?q=" + argument
+    data = urllib.request.urlopen(finalUrl)
+    value = float(str(data.read())[2:-3])
 
-print(subj_docs[0]) # shows the first tokenized subjective movie review
+else:
+    # empty sentence - no need to analyze it
+    value = 0.0
 
-
-train_subj_docs = subj_docs[:100]
-test_subj_docs = subj_docs[100:500]
-train_obj_docs = obj_docs[:100]
-test_obj_docs = obj_docs[100:500]
-
-training_docs = train_subj_docs + train_obj_docs
-testing_docs = test_subj_docs + test_obj_docs
-
-sentim_analyzer = SentimentAnalyzer()
-all_words_neg = sentim_analyzer.all_words([mark_negation(doc) for doc in training_docs])        # mark_negation puts a _NEG suffix to words that appear between a negation (not, didn't...) and a punction mark
-
-unigram_feats = sentim_analyzer.unigram_word_feats(all_words_neg, min_freq=4)                   # returns the top words/tokens sorted by frequency
-
-sentim_analyzer.add_feat_extractor(extract_unigram_feats, unigrams=unigram_feats)
-
-training_set = sentim_analyzer.apply_features(training_docs)
-test_set = sentim_analyzer.apply_features(testing_docs)
-
-trainer = NaiveBayesClassifier.train
-classifier = sentim_analyzer.train(trainer, training_set)
-
-for key, value in sorted(sentim_analyzer.evaluate(test_set).items()):
-    print('{0}: {1}'.format(key, value))
+print(value)
